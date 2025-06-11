cbe2f69980234dd69d9d75aa72493247f5e1ae9b
levlong <hi.newagenewera@gmail.com>
2025-06-11 09:44:40 +0700

    [INIT] initialize project

---
+++ src/main/java/com/thelong/longsmanor/LongsmanorApplication.java
@@ -0,0 +1,13 @@
+package com.thelong.longsmanor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongsmanorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongsmanorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thelong/longsmanor/LongsmanorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thelong.longsmanor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongsmanorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
