e535d9373d3f36769bb6bf9a2bc3646cc37f27b2
levlong <hi.newagenewera@gmail.com>
2025-06-19 10:51:26 +0700

    [ADD] ping api

---
+++ src/main/java/com/thelong/longsmanor/controller/KeepConnectionController.java
@@ -0,0 +1,25 @@
+package com.thelong.longsmanor.controller;
+
+import com.thelong.longsmanor.dto.PingResponse;
+import com.thelong.longsmanor.service.KeepConnection;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KeepConnectionController {
+    
+    @Autowired
+    private KeepConnection service;
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/api/monitor")
+    public List<PingResponse> monitor() {
+        return service.getLogs();
+    }
+    
+}
--- src/main/java/com/thelong/longsmanor/service/KeepConnection.java
+++ src/main/java/com/thelong/longsmanor/service/KeepConnection.java
@@ -1,5 +1,6 @@
 package com.thelong.longsmanor.service;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,32 +9,38 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.thelong.longsmanor.dto.PingResponse;
+
 @Component
 public class KeepConnection {
 
     private static final String URL = "https://webhook-hi7l.onrender.com/wake-up";
     private final RestTemplate restTemplate = new RestTemplate();
-    private final List<String> logs = Collections.synchronizedList(new LinkedList<>());
+    private final List<PingResponse> logs = Collections.synchronizedList(new LinkedList<>());
 
     @Scheduled(fixedRate = 600000)
     public void awake() {
         try {
             String response = restTemplate.getForObject(URL, String.class);
-            addLogs("✅SUCCESS at " + java.time.LocalDateTime.now() + " - " +"Response from server: " + response);
+            if (response.isEmpty()) {
+                addLogs(new PingResponse("❌Fail", LocalDateTime.now(), response));
+            }
+            addLogs(new PingResponse("✅Success", LocalDateTime.now(), response));
+
         } catch (Exception e) {
-            addLogs("❌FAIL at " + java.time.LocalDateTime.now() + ": " + e.getMessage());
+            
         }
     }
 
-    public void addLogs(String msg) {
+    public void addLogs(PingResponse response) {
         if (logs.size() > 100) {
             logs.remove(0);
         }
 
-        logs.add(msg);
+        logs.add(response);
     }
 
-    public List<String> getLogs() {
+    public List<PingResponse> getLogs() {
         return logs;
     }
 }
