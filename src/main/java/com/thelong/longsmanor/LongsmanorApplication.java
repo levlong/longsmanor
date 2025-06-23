275749dcea86d4b4da0c567a299a071736f6ec07
levlong <hi.newagenewera@gmail.com>
2025-06-23 09:53:49 +0700

    [ADD] add cors

--- src/main/java/com/thelong/longsmanor/LongsmanorApplication.java
+++ src/main/java/com/thelong/longsmanor/LongsmanorApplication.java
@@ -2,7 +2,10 @@ package com.thelong.longsmanor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,16 @@ public class LongsmanorApplication {
 		SpringApplication.run(LongsmanorApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://thelong.xyz")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
--- src/main/java/com/thelong/longsmanor/controller/KeepConnectionController.java
+++ src/main/java/com/thelong/longsmanor/controller/KeepConnectionController.java
@@ -6,7 +6,6 @@ import com.thelong.longsmanor.service.KeepConnection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,6 @@ public class KeepConnectionController {
     @Autowired
     private KeepConnection service;
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/api/monitor")
     public List<PingResponse> monitor() {
         return service.getLogs();
