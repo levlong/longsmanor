ee38fa764613525aa343a76e57ebd0346552220e
levlong <hi.newagenewera@gmail.com>
2025-07-01 09:17:40 +0700

    [ADD] filter api

--- src/main/java/com/thelong/longsmanor/controller/KeepConnectionController.java
+++ src/main/java/com/thelong/longsmanor/controller/KeepConnectionController.java
@@ -3,11 +3,15 @@ package com.thelong.longsmanor.controller;
 import com.thelong.longsmanor.model.Ping;
 import com.thelong.longsmanor.repository.PingRepository;
 
+import lombok.Data;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,22 +23,70 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api")
+@Data
 public class KeepConnectionController {
 
-    @Autowired
     private PingRepository repository;
 
+    /**
+     * Get all items
+     * 
+     * @return
+     */
     @GetMapping("/monitors")
     public CollectionModel<EntityModel<Ping>> all() {
-        List<EntityModel<Ping>> pings = repository.findAll().stream().map(ping -> EntityModel.of(ping,
-                linkTo(methodOn(KeepConnectionController.class).one(ping.get_id())).withSelfRel(),
-                linkTo(methodOn(KeepConnectionController.class).all()).withRel("all-logs")))
+        List<EntityModel<Ping>> pings = repository.findAll().stream()
+                .map(ping -> EntityModel.of(ping,
+                        linkTo(methodOn(KeepConnectionController.class).one(ping.get_id())).withSelfRel(),
+                        linkTo(methodOn(KeepConnectionController.class).all()).withRel("all-logs")))
                 .collect(Collectors.toList());
         return CollectionModel.of(pings,
-            linkTo(methodOn(KeepConnectionController.class).all()).withSelfRel()
-        );
+                linkTo(methodOn(KeepConnectionController.class).all()).withSelfRel());
     }
 
+    /**
+     * Get 10 lastest items
+     * 
+     * @param timePinged
+     * @return
+     */
+    @GetMapping("/monitors/latest")
+    public CollectionModel<EntityModel<Ping>> get10() {
+        List<EntityModel<Ping>> top10 = repository.findTop10ByOrderByTimePingedDesc().stream()
+                .map(ping -> EntityModel.of(ping,
+                        linkTo(methodOn(KeepConnectionController.class).one(ping.get_id())).withRel("one"),
+                        linkTo(methodOn(KeepConnectionController.class).get10()).withRel("top-10"),
+                        linkTo(methodOn(KeepConnectionController.class).all()).withRel("all-logs")))
+                .collect(Collectors.toList());
+        return CollectionModel.of(top10,
+                linkTo(methodOn(KeepConnectionController.class).get10()).withSelfRel());
+    }
+
+    /**
+     * Get n latest items
+     * @param n
+     * @return
+     */
+    @GetMapping("/monitors/{n}-latest")
+    public CollectionModel<EntityModel<Ping>> getN(@PathVariable int n) {
+
+        Pageable pageable = PageRequest.of(0, n, Sort.by("timePinged").descending());
+
+        List<EntityModel<Ping>> top10 = repository.findAllByOrderByTimePingedDesc(pageable).stream()
+                .map(ping -> EntityModel.of(ping,
+                        linkTo(methodOn(KeepConnectionController.class).one(ping.get_id())).withRel("one"),
+                        linkTo(methodOn(KeepConnectionController.class).all()).withRel("all-logs")))
+                .collect(Collectors.toList());
+        return CollectionModel.of(top10,
+                linkTo(methodOn(KeepConnectionController.class).getN(n)).withSelfRel());
+    }
+
+    /**
+     * Get item by id
+     * 
+     * @param id
+     * @return
+     */
     @GetMapping("/monitor/{id}")
     public EntityModel<Ping> one(@PathVariable ObjectId id) {
         Ping ping = repository.findBy_id(id);
--- src/main/java/com/thelong/longsmanor/repository/PingRepository.java
+++ src/main/java/com/thelong/longsmanor/repository/PingRepository.java
@@ -1,6 +1,9 @@
 package com.thelong.longsmanor.repository;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,7 @@ import com.thelong.longsmanor.model.Ping;
 @Repository
 public interface PingRepository extends MongoRepository<Ping, ObjectId>{
     Ping findBy_id(ObjectId _id);
+    List<Ping> findTop10ByOrderByTimePingedDesc();
+    List<Ping> findAllByOrderByTimePingedDesc(Pageable pageable);
+    List<Ping> findTop10ByOrderByTimePinged();
 }
